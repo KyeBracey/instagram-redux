86bff160730543117c2bca51f9202d15e61c0a86
Kye Bracey <kye.bracey@gmail.com>
2017-08-21 17:22:45 +0100

    Adds posts model with title (string) and message (text) properties

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { is_expected.to be }
+
+  it 'can be created without a message' do
+    post = Post.create(title: 'Test post')
+    expect(post).to be_valid
+  end
+
+  it 'cannot be created without a title' do
+    post = Post.create(message: 'I am doomed to fail')
+    expect(post).not_to be_valid
+  end
+end
